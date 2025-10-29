a1ad3bf4b974f5d340eddd8809a33f3e4d7d35d1
Jesús <jesuslopj0@gmail.com>
2025-10-29 20:57:33 +0100

    Cambiando ruta STATIC_ROOT

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
